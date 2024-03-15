ba11df43a7ef10939a80f41cca5026dd842c8aa6
Camille <dev@camimaki.io>
2024-03-15 23:39:22 +0100

    first commit

---
+++ PoppyTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PoppyTracker
+//
+//  Created by Camille on 15/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PoppyTracker/PoppyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoppyTrackerApp.swift
+//  PoppyTracker
+//
+//  Created by Camille on 15/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PoppyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
